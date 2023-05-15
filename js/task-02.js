542833e5ba87a021d8329ce1b5f6d4709dba3093
MaksymBora <maxboraod@gmail.com>
2023-05-15 14:55:54 +0200

    add 2nd homework

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listEl = document.getElementById('ingredients');
+
+const elements = ingredients.map(element => {
+  const createEl = document.createElement('li');
+  createEl.classList.add('item');
+  createEl.textContent = element;
+  listEl.append(createEl);
+  return createEl;
+});
+
+console.log(listEl);
