d653991fcd4e487220733766f51f65da6ed537b9
MaksymBora <maxboraod@gmail.com>
2023-05-18 15:40:07 +0200

    add 10th hw

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  input: document.querySelector('#controls > input'),
+  create: document.querySelector('#controls [data-create]'),
+  remove: document.querySelector('#controls [data-destroy]'),
+  box: document.querySelector('#boxes'),
+};
+
+refs.input.addEventListener('change', onChangeValue);
+let amount = 0;
+
+function onChangeValue(event) {
+  amount = event.target.value;
+  // console.log('Total Boxes: ', amount);
+}
+
+refs.create.addEventListener('click', createBoxes);
+refs.remove.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  let width = 30;
+  let height = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const createEl = document.createElement('div');
+    width += 10;
+    height += 10;
+    createEl.classList.add('box-item');
+    createEl.style.background = getRandomHexColor();
+    createEl.style.width = `${width}px`;
+    createEl.style.height = `${height}px`;
+
+    refs.box.append(createEl);
+  }
+}
+
+function destroyBoxes() {
+  console.log('remove');
+  const findBoxes = document.querySelectorAll('.box-item');
+  findBoxes.forEach(box => {
+    box.remove();
+  });
+}
