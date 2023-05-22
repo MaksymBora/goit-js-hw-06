596c3528080e80941f42cb9f97cc71f6a9939ad5
MaksymBora <maxboraod@gmail.com>
2023-05-22 14:49:21 +0200

    update 6th hw

--- js/task-06.js
+++ js/task-06.js
@@ -4,11 +4,13 @@ input.addEventListener('blur', inInputBlur);
 
 function inInputBlur(event) {
   const inputValue = event.target.value;
+  const requiredLength = input.dataset.length;
 
-  if (inputValue.length >= 6) {
-    input.classList.add('invalid');
-  } else {
+  if (inputValue.length === Number(requiredLength)) {
+    input.classList.remove('invalid');
     input.classList.add('valid');
+  } else {
+    input.classList.remove('valid');
+    input.classList.add('invalid');
   }
-  console.log(inputValue.length);
 }
